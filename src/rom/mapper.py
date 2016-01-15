fdc9692de4f9652e0133ab7a6298af3e8256713e
chenyan <chenyan@baijiahulian.com>
2016-01-15 20:14:30 +0800

    +临时提交

--- src/rom/mapper.py
+++ src/rom/mapper.py
@@ -1,1 +1,24 @@
 # coding=utf-8
+
+
+class Mapper(object):
+    """
+    Mapper interface
+    """
+    def read(self, a):
+        raise NotImplemented()
+
+    def write(self, a, v):
+        raise NotImplemented()
+
+    def read_vram(self, a):
+        raise NotImplemented()
+
+    def write_vram(self, a, v):
+        raise NotImplemented()
+
+    def read_tile(self):
+        raise NotImplemented()
+
+    def is_battery_backed(self):
+        raise NotImplemented()
