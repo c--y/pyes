e2d7aa80f5e220d82bc19fa2d84578f436fab814
chenyan <cfmonkey@gmail.com>
2016-01-19 23:04:55 +0800

    删除名字

--- src/rom/Mmc3.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from rom.mapper import Mapper
-
-
-class Mmc3(Mapper):
-
-    def __init__(self):
-        pass
--- src/rom/UNRom.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-
-
-class UNRom(Mapper):
-
-    def __init__(self):
-        pass
